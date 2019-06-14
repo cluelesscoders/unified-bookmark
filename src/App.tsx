af38998b5df4b0cacb328ff5b8b8a700fa2dcde1
Kunal Mangaraj <mangaraj.kunal@gmail.com>
2019-06-14 11:30:57 +0530

    testing chrome bookmarks api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 export class App extends React.Component {
+  public componentDidMount() {
+    // chrome.bookmarks.getTree(data => {
+    //   // tslint:disable-next-line:no-console
+    //   console.log(data);
+    // });
+  }
   public render() {
     return (
       <div className="App">
